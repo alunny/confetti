50ebe93968285654cdaceaad8294b6f720de0890
alunny <alunny@gmail.com>
2010-10-19 14:39:49 -0700

    author class

--- lib/confetti/config.rb
+++ lib/confetti/config.rb
@@ -1,5 +1,13 @@
 module Confetti
   class Config
     attr_accessor :name, :package, :version, :description
+    attr_reader :author
+
+    def initialize
+      @author = Author.new
+    end
+
+    class Author
+    end
   end
 end
