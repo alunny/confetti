7daa732d3890123651a66c5f399446cb8bbb2a59
alunny <andrew.lunny@nitobi.com>
2010-10-28 22:26:14 -0700

    describing the author class

---
+++ spec/config_author_spec.rb
@@ -0,0 +1,22 @@
+require 'confetti'
+
+describe Confetti::Config::Author do
+  before do
+    @author = Confetti::Config::Author.new
+  end
+
+  it "should have a readable and writable name field" do
+    lambda { @author.name = "Andrew Lunny" }.should_not raise_error
+    @author.name.should == "Andrew Lunny"
+  end
+
+  it "should have a readable and writable href field" do
+    lambda { @author.href = "http://alunny.github.com" }.should_not raise_error
+    @author.href.should == "http://alunny.github.com"
+  end
+
+  it "should have a readable and writable email field" do
+    lambda { @author.email = "alunny@gmail.com" }.should_not raise_error
+    @author.email.should == "alunny@gmail.com"
+  end
+end
