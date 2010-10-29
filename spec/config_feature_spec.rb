b3d747983af20ab989d0469738934f68b507b866
alunny <andrew.lunny@nitobi.com>
2010-10-29 13:08:13 -0700

    pending specs for param objects

--- spec/config_feature_spec.rb
+++ spec/config_feature_spec.rb
@@ -14,4 +14,12 @@ describe Confetti::Config::Feature do
     lambda { @feature.required = true }.should_not raise_error
     @feature.required.should be_true
   end
+
+  describe "parameter set" do
+    it "should have a param_set field, that is a TypedSet"
+
+    it "should make the param_set set typed to Confetti::Config::Feature::Param"
+
+    it "should not allow the param_set to be clobbered"
+  end
 end
