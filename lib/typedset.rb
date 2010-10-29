fdde2058c4eb8f75a142919bfacd44155a6b355d
alunny <andrew.lunny@nitobi.com>
2010-10-29 02:36:13 -0700

    first pass at TypedSet
    for attributes on config: IconSet, FeatureSet, etc

---
+++ lib/typedset.rb
@@ -0,0 +1,20 @@
+require "set"
+
+# based on RestricedSet from Ruby's stdlib, cf
+# http://github.com/ruby/ruby/blob/72ef219804e5524848170a197887a95718334e2a/lib/set.rb#L627-717
+# not sure why it's been commented out
+# I should follow these things
+
+class TypedSet < Set
+  def initialize (klass, *args)
+    raise ArgumentError, "require a Class object" unless klass.class == Class
+    @klass = klass
+    super args
+  end
+
+  def add o
+    raise ArgumentError, "members must be of class #{@klass}" unless o.class == @klass
+    super o
+  end
+  alias << add
+end
---
+++ spec/typedset_spec.rb
@@ -0,0 +1,68 @@
+require "typedset"
+
+describe TypedSet do
+  it "should fail without a class being passed in" do
+    lambda { TypedSet.new "foo" }.should raise_error ArgumentError
+  end
+
+  it "should succeed when a class is passed in" do
+    lambda { TypedSet.new String }.should_not raise_error
+  end
+
+  it "should allow initialization with values" do
+    lambda { 
+      TypedSet.new(String, "one", "two", "three")
+    }.should_not raise_error
+  end
+
+  it "should not allow initialization with invalid values" do
+    lambda { 
+      TypedSet.new(String, "one", [1, "two"], "three")
+    }.should raise_error ArgumentError
+  end
+
+  describe "when defined with a class" do
+    before do
+      @new_set = TypedSet.new String
+    end
+
+    it "should not allow #add with different classes" do
+      lambda { @new_set.add 12 }.should raise_error ArgumentError
+    end
+
+    it "should allow #add with the right class" do
+      lambda { @new_set.add "string" }.should_not raise_error
+    end
+
+    it "should add an object of the right class to the set using #add" do
+      @new_set.add "foo"
+      @new_set.should include "foo"
+    end
+
+    it "should not allow #<< with different classes" do
+      lambda { @new_set << 12 }.should raise_error ArgumentError
+    end
+
+    it "should allow #<< with the right class" do
+      lambda { @new_set << "string" }.should_not raise_error
+    end
+
+    it "should add an object of the right class to the set using #<<" do
+      @new_set << "foo"
+      @new_set.should include "foo"
+    end
+
+    it "should not allow #add? with different classes" do
+      lambda { @new_set.add? 12 }.should raise_error ArgumentError
+    end
+
+    it "should allow #add? with the right class" do
+      lambda { @new_set.add? "string" }.should_not raise_error
+    end
+
+    it "should add an object of the right class to the set using #<<" do
+      @new_set.add? "foo"
+      @new_set.should include "foo"
+    end
+  end
+end
