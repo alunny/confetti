e99b671cd6b030e945acd0db6d0d3d671e542398
alunny <andrew.lunny@nitobi.com>
2010-10-29 13:23:44 -0700

    refactoring/formatting for fun and profit

--- lib/confetti/config.rb
+++ lib/confetti/config.rb
@@ -4,43 +4,24 @@ module Confetti
     attr_reader :author, :viewmodes, :name, :license, :content, 
                 :icon_set, :feature_set, :preference_set
 
+    # classes that represent child elements
+    Author      = Class.new Struct.new(:name, :href, :email)
+    Name        = Class.new Struct.new(:name, :shortname)
+    License     = Class.new Struct.new(:text, :href)
+    Content     = Class.new Struct.new(:src, :type, :encoding)
+    Icon        = Class.new Struct.new(:src, :height, :width)
+    Feature     = Class.new Struct.new(:name, :required)
+    Preference  = Class.new Struct.new(:name, :value, :readonly)
+
     def initialize
-      @author       = Author.new
-      @name         = Name.new
-      @license      = License.new
-      @content      = Content.new
-      @icon_set     = TypedSet.new Icon
-      @feature_set  = TypedSet.new Feature
-      @preference_set  = TypedSet.new Preference
-      @viewmodes    = []
-    end
-
-    class Author
-      attr_accessor :name, :href, :email
-    end
-
-    class Name
-      attr_accessor :name, :shortname
-    end
-
-    class License
-      attr_accessor :text, :href
-    end
-
-    class Content
-      attr_accessor :src, :type, :encoding
-    end
-
-    class Icon
-      attr_accessor :src, :height, :width
-    end
-
-    class Feature
-      attr_accessor :name, :required
-    end
-
-    class Preference
-      attr_accessor :name, :value, :readonly
+      @author           = Author.new
+      @name             = Name.new
+      @license          = License.new
+      @content          = Content.new
+      @icon_set         = TypedSet.new Icon
+      @feature_set      = TypedSet.new Feature
+      @preference_set   = TypedSet.new Preference
+      @viewmodes        = []
     end
   end
 end
