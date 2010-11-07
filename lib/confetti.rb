08992ae885747897e639792efbf33c1a4c1a966d
alunny <andrew.lunny@nitobi.com>
2010-11-06 17:25:06 -0700

    fixing dependencies

--- lib/confetti.rb
+++ lib/confetti.rb
@@ -1,5 +1,15 @@
-require 'typedset'
+# external dependencies
+begin
+  require "bundler/setup"
+rescue LoadError
+  require "rubygems"
+  require "bundler/setup"
+end
 
+Bundler.require :default
+
+# internal dependencies
+require 'typedset'
 require 'confetti/version'
 require 'confetti/template'
 require 'confetti/config'
