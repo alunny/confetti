33d7833a12bc5a0aae0a725383f25bb626ac2223
alunny <andrew.lunny@nitobi.com>
2010-11-06 18:40:42 -0700

    confetti::templates::android spec

--- spec/templates/android_spec.rb
+++ spec/templates/android_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
 describe Confetti::Template::Android do
+  include HelpfulPaths
+
+  before :all do
+    AndroidTemplate = Confetti::Template::Android
+  end
+
   it "should inherit from the base template" do
-    Confetti::Template::Android.superclass.should be Confetti::Template::Base
+    AndroidTemplate.superclass.should be Confetti::Template::Base
+  end
+
+  it "should have the template_file \"android.mustache\" in the confetti/templates dir" do
+    AndroidTemplate.template_file.should == "#{ templates_dir }/android.mustache"
   end
 end
