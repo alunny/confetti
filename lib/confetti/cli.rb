aa9807c514495e1bfcb45441d41a750141ac31f1
alunny <alunny@gmail.com>
2010-11-23 16:50:07 -0800

    shell of a something

--- lib/confetti/cli.rb
+++ lib/confetti/cli.rb
@@ -2,6 +2,12 @@ require "confetti"
 
 module Confetti
   class CLI < Thor
-
+    desc "generate OUTPUT_FILE", "Generates a config file of type OUTPUT_FILE"
+    def generate(output_file)
+      puts Dir.pwd
+      # find config.xml in the current directory
+      # read config.xml
+      # write out android_(whatever) to current directory
+    end
   end
 end
