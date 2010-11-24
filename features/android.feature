7ad0afa087998ebf42746df977f1167e04b703b6
alunny <andrew.lunny@nitobi.com>
2010-11-23 22:02:51 -0800

    simplifying android acceptance test
    the unit tests cover the contents of the file

--- features/android.feature
+++ features/android.feature
@@ -4,7 +4,6 @@ Feature: Android
 	I want to generate Android configuration files
 
 	Scenario: I need an AndroidManifest
-		When I'm in the fixtures directory
+		When I have a config.xml file
 		And I run "confetti generate android_manifest"
 		Then a file named "AndroidManifest.xml" should exist
-		And the "AndroidManifest.xml" file should be identical to "AndroidManifest_expected.xml"
--- features/step_definitions/aruba_ext_steps.rb
+++ features/step_definitions/aruba_ext_steps.rb
@@ -1,11 +1,7 @@
-When /^I'm in the fixtures directory$/ do
-  @dirs = []
+When /^I have a config.xml file$/ do
   fixtures_dir = File.join(File.dirname(__FILE__), '..', '..', 'spec', 'fixtures')
   fixtures_dir = File.expand_path(fixtures_dir)
-  cd fixtures_dir
-end
+  config_fixture = File.read(File.join(fixtures_dir, "config.xml"))
 
-Then /^the "([^"]*)" file should be identical to "([^"]*)"$/ do |file_one, file_two|
-  other_file = File.read(File.join(current_dir, file_two))
-  check_exact_file_content(file_one, other_file)
+  create_file("config.xml", config_fixture)
 end
