a6a794835ff7f243931c590ac8fe6820c37cb47e
alunny <andrew.lunny@nitobi.com>
2010-11-23 18:52:47 -0800

    taking advantage of default output_filename

--- lib/confetti/cli.rb
+++ lib/confetti/cli.rb
@@ -5,10 +5,9 @@ module Confetti
     desc "generate OUTPUT_FILE", "Generates a config file of type OUTPUT_FILE"
     def generate(output_file)
       config_xml = File.join(Dir.pwd, "config.xml")
-      output_name = File.join(Dir.pwd, "AndroidManifest.xml")
 
       config = Confetti::Config.new(config_xml)
-      config.write_android_manifest(output_name)
+      config.write_android_manifest
     end
   end
 end
