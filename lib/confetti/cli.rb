470fd22ac65021a6bbb03b53e58168006c212015
alunny <alunny@gmail.com>
2010-11-23 16:57:20 -0800

    build AndroidManifest from CLI

--- lib/confetti/cli.rb
+++ lib/confetti/cli.rb
@@ -4,10 +4,11 @@ module Confetti
   class CLI < Thor
     desc "generate OUTPUT_FILE", "Generates a config file of type OUTPUT_FILE"
     def generate(output_file)
-      puts Dir.pwd
-      # find config.xml in the current directory
-      # read config.xml
-      # write out android_(whatever) to current directory
+      config_xml = File.join(Dir.pwd, "config.xml")
+      output_name = File.join(Dir.pwd, "AndroidManifest.xml")
+
+      config = Confetti::Config.new(config_xml)
+      config.write_android_manifest(output_name)
     end
   end
 end
