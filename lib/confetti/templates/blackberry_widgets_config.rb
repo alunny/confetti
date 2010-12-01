3bd9c74f766e8b285a0aa05d3b00b3556d4b2d21
alunny <alunny@gmail.com>
2010-12-01 11:08:18 -0800

    blackberry widgets boilerplate

---
+++ lib/confetti/templates/blackberry_widgets_config.rb
@@ -0,0 +1,37 @@
+module Confetti
+  module Template
+    class BlackberryWidgetsConfig < Base
+      def widget_version
+        @config.version
+      end
+
+      def widget_id
+        @config.package
+      end
+
+      def widget_name
+        @config.name.name
+      end
+
+      def author_href
+        @config.author.href
+      end
+
+      def author_email
+        @config.author.email
+      end
+
+      def author_name
+        @config.author.name
+      end
+
+      def widget_description
+        @config.description
+      end
+
+      def output_filename
+        "bb-config.xml"
+      end
+    end
+  end
+end
---
+++ spec/templates/blackberry_widget_config_spec.rb
@@ -0,0 +1,94 @@
+require 'spec_helper'
+
+describe Confetti::Template::BlackberryWidgetsConfig do
+  include HelpfulPaths
+
+  before :all do
+    @template_class = Confetti::Template::BlackberryWidgetsConfig
+  end
+
+  it "should inherit from the base template" do
+    @template_class.superclass.should be Confetti::Template::Base
+  end
+
+  it "should have the template_file \"blackberry_widgets_config.mustache\" in the confetti/templates dir" do
+    @template_class.template_file.should == "#{ templates_dir }/blackberry_widgets_config.mustache"
+  end
+
+  describe "templated attributes" do
+    subject { @template = @template_class.new }
+
+    it { should respond_to :widget_version }
+    it { should respond_to :widget_id }
+    it { should respond_to :widget_name }
+    it { should respond_to :author_name }
+    it { should respond_to :author_email }
+    it { should respond_to :author_href }
+    it { should respond_to :widget_description }
+  end
+
+  describe "default values" do
+    it "should define output filename as \"bb-config.xml\"" do
+      @template_class.new.output_filename.should == "bb-config.xml"
+    end
+  end
+
+  describe "when passed a config object" do
+    before do
+      @config = Confetti::Config.new
+      @config.name.name = "Awesome App"
+      @config.package = "com.whoever.awesome.app"
+      @config.version = "1.0.0"
+      @config.author.name = "Bruce Lee"
+      @config.author.email = "blee@hotmail.com"
+      @config.author.href = "http://brucelee.cn"
+      @config.description = "My New App, by Bruce Lee"
+    end
+
+    it "should accept the config object" do
+      lambda {
+        @template_class.new(@config)
+      }.should_not raise_error
+    end
+
+    describe "templated attributes" do
+      before do
+        @template = @template_class.new(@config)
+      end
+
+      it "should set identifier correctly" do
+        @template.widget_id.should == "com.whoever.awesome.app"
+      end
+
+      it "should set display_name correctly" do
+        @template.widget_name.should == "Awesome App"
+      end
+
+      it "should set version correctly" do
+        @template.widget_version.should == "1.0.0"
+      end
+
+      it "should set description correctly" do
+        @template.widget_description.should == "My New App, by Bruce Lee"
+      end
+
+      describe "author" do
+        it "should set name correctly" do
+          @template.author_name.should == "Bruce Lee"
+        end
+
+        it "should set href correctly" do
+          @template.author_href.should == "http://brucelee.cn"
+        end
+
+        it "should set email correctly" do
+          @template.author_email.should == "blee@hotmail.com"
+        end
+      end
+
+      it "should render the correct config.xml" do
+        @template.render.should == File.read("#{ fixture_dir }/blackberry_widget_config_spec.xml")
+      end
+    end
+  end
+end
