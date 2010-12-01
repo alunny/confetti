a1297f7407b162744feaa8c0aa6377b0b491ad52
alunny <alunny@gmail.com>
2010-11-30 16:54:40 -0800

    renaming webos appinfo spec

--- spec/templates/webos_appinfo_spec.rb
+++ spec/templates/webos_appinfo_spec.rb
@@ -57,7 +57,7 @@ describe Confetti::Template::WebosAppinfo do
       end
 
       it "should render the correct AndroidManifest" do
-        @template.render.should == File.read("#{ fixture_dir }/webos_appinfo_expected.json")
+        @template.render.should == File.read("#{ fixture_dir }/webos_appinfo_spec.json")
       end
 
       describe "#version method" do
