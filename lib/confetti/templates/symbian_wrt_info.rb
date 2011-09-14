f395c1b73a797256e34b653b5842e7802ccef333
Hardeep Shoker <hardeep.shoker@nitobi.com>
2011-09-14 09:22:14 -0700

    changing symbian to include network by default

--- lib/confetti/templates/symbian_wrt_info.rb
+++ lib/confetti/templates/symbian_wrt_info.rb
@@ -26,21 +26,6 @@ module Confetti
         "info.plist"
       end
 
-      def permissions
-        permissions = []
-        phonegap_api = /http\:\/\/api.phonegap.com\/1[.]0\/(\w+)/
-        feature_names = @config.feature_set.map { |f| f.name }
-        feature_names.sort
-
-        feature_names.each do |f|
-          feature_name = f.match(phonegap_api)[1] if f.match(phonegap_api)
-          associated_permissions = GAP_PERMISSIONS_MAP[feature_name]
-          permissions.concat(associated_permissions) if associated_permissions
-        end
-
-        permissions.sort!
-        permissions.map { |f| { :name => f } }
-      end
     end
   end
 end
