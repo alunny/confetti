a64100c5f3fb11d862bd38b060d6761339fcae4c
alunny <alunny@gmail.com>
2011-09-28 14:21:00 -0700

    clean up TypedSet class

--- lib/typedset.rb
+++ lib/typedset.rb
@@ -1,22 +1,19 @@
 require "set"
 
-# based on RestricedSet from Ruby's stdlib, cf
-# http://github.com/ruby/ruby/blob/72ef219804e5524848170a197887a95718334e2a/lib/set.rb#L627-717
-# not sure why it's been commented out
-# I should follow these things
-
 class TypedSet < Set
-  attr_reader :klass
-  alias set_class klass
+  attr_reader :set_class
 
   def initialize (klass, *args)
-    raise ArgumentError, "require a Class object" unless klass.class == Class
-    @klass = klass
+    raise ArgumentError, "require a class object" unless klass.class == Class
+    @set_class = klass
     super args
   end
 
   def add o
-    raise ArgumentError, "members must be of class #{@klass}" unless o.class == @klass
+    unless o.class == @set_class
+      raise ArgumentError, "members must be of class #{ @set_class }"
+    end
+
     super o
   end
   alias << add
